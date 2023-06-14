70099e2ae37636a298b0c601be45cda47114644e
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-14 14:24:49 +0800

    added input scanner

---
+++ Main.java
@@ -0,0 +1,24 @@
+// Problem Statement
+// Using Java Stack, create a program that will determine whether a set of parenthesis ((,{) is balance
+// or not
+// Input
+// Input starts with a number N and is followed by N sets of parenthesis
+// Output
+// Print BALANCED in the parenthesis is balanced, NOT BALANCED otherwise.
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static main(String args[]){
+        Stack<String> stackLeft = new Stack<String>();
+        Stack<String> stackRight = new Stack<String>();
+        Scanner inputScan = new Scanner(System.in);
+
+        System.out.Println("Enter Input:")
+        String inputGet = inputScan.nextLine();
+
+
+
+    }
+}
